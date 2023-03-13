8731b2a9fb4edc66d770b7da93eabba9fee34b73
RomanKamko <r.kamko@mail.ru>
2023-03-13 15:57:22 +0400

    commit

---
+++ src/test/java/com/kamko/kamkospringdemo/KamkoSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamko.kamkospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamkoSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
